9dd6cc0a86a43160ccaf448ad0f05417b76465f9
ShaneLau2 <xin.liu@maine.edu>
2024-03-07 01:18:41 +0000

    a0

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArr) {
+    const bookTitle = [];
+    bookArr.map((book) => bookTitle.push(book.title));
+    return bookTitle;
 };
 
 // Do not edit below this line
