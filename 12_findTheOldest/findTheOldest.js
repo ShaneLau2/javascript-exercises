0a260497bb9cc336291982782e647f70e59dff34
ShaneLau2 <xin.liu@maine.edu>
2024-03-07 22:02:44 +0000

    finish all the test cases, and i learned the reduce method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        let currentAge;
+        if (!currentPerson.yearOfDeath) {
+            currentAge = new Date().getFullYear() - currentPerson.yearOfBirth;
+        } else {
+            currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        }
+        if (!oldest) {
+            oldest = { name: currentPerson.name, year: currentAge };
+        }
+        if (currentAge > oldest.year) {
+            oldest = { name: currentPerson.name, year: currentAge };
+        }
+        return oldest;
+    }, null);
 };
 
 // Do not edit below this line
